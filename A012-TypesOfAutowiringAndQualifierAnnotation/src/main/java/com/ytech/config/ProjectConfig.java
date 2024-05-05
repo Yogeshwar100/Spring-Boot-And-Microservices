11330e809e2cd88a0ff327611c18279bd9c2176d
Yogeshwar Jamdade <yogeshwar473@gmail.com>
2024-05-05 21:36:52 +0530

    TypesOfAutowiringAndQualifierAnnotation

---
+++ A012-TypesOfAutowiringAndQualifierAnnotation/src/main/java/com/ytech/config/ProjectConfig.java
@@ -0,0 +1,36 @@
+package com.ytech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.ytech.beans.Person;
+import com.ytech.beans.Vehicle;
+
+@Configuration
+@ComponentScan(basePackages = "com.ytech.beans")
+public class ProjectConfig {
+
+    @Bean
+    Vehicle vehicle1() {
+        var veh = new Vehicle();
+        veh.setName("Audi");
+        return veh;
+    }
+
+    @Bean
+    Vehicle vehicle2() {
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Bean
+    Vehicle vehicle3() {
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+
+}
