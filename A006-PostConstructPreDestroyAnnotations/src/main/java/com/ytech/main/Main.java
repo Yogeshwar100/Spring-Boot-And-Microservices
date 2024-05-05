8632c2377bc5f05fc7c0f6320995d9aee156a619
Yogeshwar Jamdade <yogeshwar473@gmail.com>
2024-05-05 19:27:30 +0530

    PostConstruct & PreDestroy Annotations

---
+++ A006-PostConstructPreDestroyAnnotations/src/main/java/com/ytech/main/Main.java
@@ -0,0 +1,19 @@
+package com.ytech.main;
+
+import com.ytech.beans.Vehicle;
+import com.ytech.config.ProjectConfig;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+		Vehicle vehicle = context.getBean(Vehicle.class);
+		System.out.println("Component Vehicle name from " + "Spring Context is: " + vehicle.getName());
+		vehicle.printHello();
+		context.close();
+
+	}
+}
