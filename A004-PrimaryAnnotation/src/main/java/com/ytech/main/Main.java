54f7b78f54e7ac5c673c8c2ae807c4c8284aaded
Yogeshwar Jamdade <yogeshwar473@gmail.com>
2024-05-05 19:24:38 +0530

    Primary Annotation

---
+++ A004-PrimaryAnnotation/src/main/java/com/ytech/main/Main.java
@@ -0,0 +1,17 @@
+package com.ytech.main;
+
+import com.ytech.beans.Vehicle;
+import com.ytech.config.ProjectConfig;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Primary Vehicle name from Spring Context is: " + vehicle.getName());
+
+    }
+}
