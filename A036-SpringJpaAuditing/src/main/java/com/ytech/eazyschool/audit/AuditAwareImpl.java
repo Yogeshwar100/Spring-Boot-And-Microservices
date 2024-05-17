0380eaa3711f12d55369b2b532e881d4583a022d
Yogeshwar Jamdade <yogeshwar473@gmail.com>
2024-05-17 17:19:09 +0530

    Spring JPA Auditing

---
+++ A036-SpringJpaAuditing/src/main/java/com/ytech/eazyschool/audit/AuditAwareImpl.java
@@ -0,0 +1,17 @@
+package com.ytech.eazyschool.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+//@Component(value="auditAwareImpl")
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+            return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+}
---
+++ A036-SpringJpaAuditing/src/main/java/com/ytech/eazyschool/constants/EazySchoolConstants.java
@@ -0,0 +1,7 @@
+package com.ytech.eazyschool.constants;
+
+public interface EazySchoolConstants {
+
+    public static final String OPEN = "Open";
+    public static final String CLOSE = "Close";
+}
